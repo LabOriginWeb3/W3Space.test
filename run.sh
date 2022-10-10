a06532507455de328489f437bc90acfcfa3bef04
cuegod <macbook@MacdeMacBook-Pro.local>
2022-10-10 17:09:47 +0800

    50 default process

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/zsh
-#for zsh
-repeat 3 nohup node pressure.js
 
-# for bash script
-#for i in {1..3}; do
-#    nohup node pressure.js &
-#done
+for i in {1..50}; do
+    nohup node pressure.js &
+done
 
